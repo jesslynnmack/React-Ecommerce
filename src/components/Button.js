94b603b870f03eec4677904be99eca82df6b4d0b
Jessica Mccormack <jesslynn.mack@gmail.com>
2019-02-25 11:50:37 -0600

    added initial files, components, and css

---
+++ src/components/Button.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.button `
+text-transform: capitalize;
+font-size: 1.4 rem;
+background: transparent;
+border: 0.09rem solid var(--lightBlue);
+border-color:${props => props.cart? "var(--mainYellow)":"var(--lightBlue)"};
+color: ${prop => (prop.cart? "var(--mainYellow)" : "var(--lightBlue)")};
+border-radius: 0.5rem;
+padding: 0.2rem 0.5 rem;
+cursor: pointer;
+margin: 0.2rem 0.5rem;
+transition:all 0.5s ease-in-out;
+&:hover{
+    background: ${prop => prop.cart? "var(--mainYellow)":"var(--lightBlue)"};
+    color: var(--mainBlue);
+}
+&:focus{
+    outline:none;
+}
+`
